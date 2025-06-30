03b413233f3e8672b292434633ceb71a38c450ee
melaniemdm <melanieborja.pro@gmail.com>
2025-06-30 14:43:13 +0200

    first commit

---
+++ back/src/main/java/com/example/poc_chat_ycyw/PocChatYcywApplication.java
@@ -0,0 +1,13 @@
+package com.example.poc_chat_ycyw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocChatYcywApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocChatYcywApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/example/poc_chat_ycyw/PocChatYcywApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poc_chat_ycyw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocChatYcywApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
